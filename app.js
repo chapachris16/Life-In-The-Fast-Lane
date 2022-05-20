80487e481f78976a220edfc01f3ad745cefea421
christopher chapa <chapachristopher16@gmail.com>
2022-05-20 14:46:18 -0500

    Link css and js files to html

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+// Create function for players car to move up down left right based on arrow keys
+
+// Create function that is a timer for player to need to get gas(This will be a loss condition if you run out of gas)
+
+// Create a function for opposing cars(obstacles another loss condition) if player car collides with another car game over
+
+// Create function to detect game over 
+
+// Create function that causes the obstacles to move faster over time(maybe create an setInterval() )
+
+// Function for keeping track of allotted time survived in-game. At game over display how long the player survived
+
+// Maybe randomize obstacles to be a traffic cone, tire in the road, trashcan, or roadspikes
 
