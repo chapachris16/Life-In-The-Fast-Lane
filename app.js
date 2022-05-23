7ac7fc4bab0ffe74dec31f930d932df94acd7aa3
christopher chapa <chapachristopher16@gmail.com>
2022-05-23 08:52:37 -0500

    Created variables for game and made class for player car

--- app.js
+++ app.js
@@ -12,3 +12,48 @@
 
 // Maybe randomize obstacles to be a traffic cone, tire in the road, trashcan, or roadspikes
 
+// Set Variables for Game to be called
+// let playerOne
+let obstacles
+let gasCan
+let game = document.getElementById('game')
+let ctx = game.getContext('2d')
+// 
+
+// class creator for player
+class Car{
+    constructor(x,y,color,width,height){
+        this.x = x
+        this.y = y
+        this.color = color
+        this.height = height
+        this.width = width 
+        this.alive = true
+    }
+    // Method to create car on screen
+    render(){
+        ctx.fill = this.color
+        ctx.fillRect(this.x,this.y, this.width, this.height) 
+    }
+}
+// 
+let playerOne = new Car(10,400,'red',20,20)
+// function layout for movement
+let movement = ((e) =>{
+    console.log(`key pressed: ${e.key}`)
+    switch (e.key){
+        case "ArrowUp":
+            playerOne.y >= 0 ? playerOne.y -= 20: null
+            break
+        case "ArrowDown":
+            playerOne.y >= 0 ? playerOne.y += 20: null
+            break
+        case "ArrowLeft":
+            playerOne.x >= 0 ? playerOne.x -= 10: null
+            break
+        case 'ArrowRight':
+            playerOne.x >= 0 ? playerOne.x += 10: null
+            break
+    }
+        
+})
